e4e13e207fc95e1aaa6345344aa40cf1f73494d4
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-04-02 06:06:36 +0000

    ### Added - 劇場編集エンドポイント追加

--- dst/app/routes/places/movieTheater.js
+++ dst/app/routes/places/movieTheater.js
@@ -13,26 +13,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const chevre = require("@chevre/domain");
 const express_1 = require("express");
+// tslint:disable-next-line:no-submodule-imports
+const check_1 = require("express-validator/check");
+const http_status_1 = require("http-status");
 const mongoose = require("mongoose");
 const authentication_1 = require("../../middlewares/authentication");
 const permitScopes_1 = require("../../middlewares/permitScopes");
 const validator_1 = require("../../middlewares/validator");
 const movieTheaterRouter = express_1.Router();
 movieTheaterRouter.use(authentication_1.default);
+movieTheaterRouter.post('', permitScopes_1.default(['admin']), ...[
+    check_1.body('branchCode')
+        .not()
+        .isEmpty(),
+    check_1.body('name')
+        .not()
+        .isEmpty()
+], validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const movieTheater = Object.assign({}, req.body, { typeOf: chevre.factory.placeType.MovieTheater });
+        const placeRepo = new chevre.repository.Place(mongoose.connection);
+        yield placeRepo.saveMovieTheater(movieTheater);
+        res.status(http_status_1.CREATED)
+            .json(movieTheater);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
 movieTheaterRouter.get('', permitScopes_1.default(['admin', 'places', 'places.read-only']), validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
         const placeRepo = new chevre.repository.Place(mongoose.connection);
-        const searchCoinditions = {
+        const searchCoinditions = Object.assign({}, req.query, { 
             // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
-            limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
-            page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1,
-            sort: req.query.sort,
-            name: req.query.name
-        };
+            limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1 });
         const totalCount = yield placeRepo.countMovieTheaters(searchCoinditions);
         const movieTheaters = yield placeRepo.searchMovieTheaters(searchCoinditions);
-        res.set('X-Total-Count', totalCount.toString());
-        res.json(movieTheaters);
+        res.set('X-Total-Count', totalCount.toString())
+            .json(movieTheaters);
     }
     catch (error) {
         next(error);
@@ -43,6 +61,22 @@ movieTheaterRouter.get('/:branchCode', permitScopes_1.default(['admin', 'places'
         const placeRepo = new chevre.repository.Place(mongoose.connection);
         const movieTheater = yield placeRepo.findMovieTheaterByBranchCode({ branchCode: req.params.branchCode });
         res.json(movieTheater);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+movieTheaterRouter.put('/:branchCode', permitScopes_1.default(['admin']), ...[
+    check_1.body('name')
+        .not()
+        .isEmpty()
+], validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const movieTheater = Object.assign({}, req.body, { typeOf: chevre.factory.placeType.MovieTheater, branchCode: req.params.branchCode });
+        const placeRepo = new chevre.repository.Place(mongoose.connection);
+        yield placeRepo.saveMovieTheater(movieTheater);
+        res.status(http_status_1.NO_CONTENT)
+            .end();
     }
     catch (error) {
         next(error);
--- src/app/routes/places/movieTheater.ts
+++ src/app/routes/places/movieTheater.ts
@@ -3,6 +3,9 @@
  */
 import * as chevre from '@chevre/domain';
 import { Router } from 'express';
+// tslint:disable-next-line:no-submodule-imports
+import { body } from 'express-validator/check';
+import { CREATED, NO_CONTENT } from 'http-status';
 import * as mongoose from 'mongoose';
 
 import authentication from '../../middlewares/authentication';
@@ -10,7 +13,38 @@ import permitScopes from '../../middlewares/permitScopes';
 import validator from '../../middlewares/validator';
 
 const movieTheaterRouter = Router();
+
 movieTheaterRouter.use(authentication);
+
+movieTheaterRouter.post(
+    '',
+    permitScopes(['admin']),
+    ...[
+        body('branchCode')
+            .not()
+            .isEmpty(),
+        body('name')
+            .not()
+            .isEmpty()
+    ],
+    validator,
+    async (req, res, next) => {
+        try {
+            const movieTheater: chevre.factory.place.movieTheater.IPlace = {
+                ...req.body,
+                typeOf: chevre.factory.placeType.MovieTheater
+            };
+            const placeRepo = new chevre.repository.Place(mongoose.connection);
+            await placeRepo.saveMovieTheater(movieTheater);
+
+            res.status(CREATED)
+                .json(movieTheater);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 movieTheaterRouter.get(
     '',
     permitScopes(['admin', 'places', 'places.read-only']),
@@ -19,21 +53,22 @@ movieTheaterRouter.get(
         try {
             const placeRepo = new chevre.repository.Place(mongoose.connection);
             const searchCoinditions: chevre.factory.place.movieTheater.ISearchConditions = {
+                ...req.query,
                 // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
                 limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
-                page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1,
-                sort: req.query.sort,
-                name: req.query.name
+                page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
             };
             const totalCount = await placeRepo.countMovieTheaters(searchCoinditions);
             const movieTheaters = await placeRepo.searchMovieTheaters(searchCoinditions);
-            res.set('X-Total-Count', totalCount.toString());
-            res.json(movieTheaters);
+
+            res.set('X-Total-Count', totalCount.toString())
+                .json(movieTheaters);
         } catch (error) {
             next(error);
         }
     }
 );
+
 movieTheaterRouter.get(
     '/:branchCode',
     permitScopes(['admin', 'places', 'places.read-only']),
@@ -42,10 +77,39 @@ movieTheaterRouter.get(
         try {
             const placeRepo = new chevre.repository.Place(mongoose.connection);
             const movieTheater = await placeRepo.findMovieTheaterByBranchCode({ branchCode: req.params.branchCode });
+
             res.json(movieTheater);
         } catch (error) {
             next(error);
         }
     }
 );
+
+movieTheaterRouter.put(
+    '/:branchCode',
+    permitScopes(['admin']),
+    ...[
+        body('name')
+            .not()
+            .isEmpty()
+    ],
+    validator,
+    async (req, res, next) => {
+        try {
+            const movieTheater: chevre.factory.place.movieTheater.IPlace = {
+                ...req.body,
+                typeOf: chevre.factory.placeType.MovieTheater,
+                branchCode: req.params.branchCode
+            };
+            const placeRepo = new chevre.repository.Place(mongoose.connection);
+            await placeRepo.saveMovieTheater(movieTheater);
+
+            res.status(NO_CONTENT)
+                .end();
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 export default movieTheaterRouter;
