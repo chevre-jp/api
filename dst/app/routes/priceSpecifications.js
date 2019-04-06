dbcec8e094b0302129503ee700b4fa8494333ee5
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-04-06 03:35:47 +0000

    ### Added - 一時的に価格仕様エンドポイントを拡張

--- dst/app/routes/priceSpecifications.js
+++ dst/app/routes/priceSpecifications.js
@@ -13,6 +13,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const chevre = require("@chevre/domain");
 const express_1 = require("express");
+const http_status_1 = require("http-status");
 const mongoose = require("mongoose");
 const authentication_1 = require("../middlewares/authentication");
 const permitScopes_1 = require("../middlewares/permitScopes");
@@ -36,6 +37,82 @@ priceSpecificationsRouter.get('/compoundPriceSpecification', permitScopes_1.defa
         const priceSpecifications = yield priceSpecificationRepo.searchCompoundPriceSpecifications(searchCoinditions);
         res.set('X-Total-Count', totalCount.toString());
         res.json(priceSpecifications);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+priceSpecificationsRouter.post('', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        let priceSpecification = Object.assign({}, req.body);
+        const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+        const doc = yield priceSpecificationRepo.priceSpecificationModel.create(priceSpecification);
+        priceSpecification = doc.toObject();
+        res.status(http_status_1.CREATED)
+            .json(priceSpecification);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+priceSpecificationsRouter.get('/:id', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+        const doc = yield priceSpecificationRepo.priceSpecificationModel.findById(req.params.id)
+            .exec();
+        if (doc === null) {
+            throw new chevre.factory.errors.NotFound('PriceSpecification');
+        }
+        const priceSpecification = doc.toObject();
+        res.json(priceSpecification);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+priceSpecificationsRouter.put('/:id', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const priceSpecification = Object.assign({}, req.body);
+        const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+        yield priceSpecificationRepo.priceSpecificationModel.findByIdAndUpdate(req.params.id, priceSpecification)
+            .exec();
+        res.status(http_status_1.NO_CONTENT)
+            .end();
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+priceSpecificationsRouter.get('', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+        const searchCoinditions = Object.assign({}, req.query, { 
+            // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
+            limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1 });
+        const totalCount = yield priceSpecificationRepo.priceSpecificationModel.countDocuments({})
+            .setOptions({ maxTimeMS: 10000 })
+            .exec();
+        const query = priceSpecificationRepo.priceSpecificationModel.find({}, {
+            __v: 0,
+            createdAt: 0,
+            updatedAt: 0
+        });
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (searchCoinditions.limit !== undefined && searchCoinditions.page !== undefined) {
+            query.limit(searchCoinditions.limit)
+                .skip(searchCoinditions.limit * (searchCoinditions.page - 1));
+        }
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (searchCoinditions.sort !== undefined) {
+            query.sort(searchCoinditions.sort);
+        }
+        const priceSpecifications = yield query.setOptions({ maxTimeMS: 10000 })
+            .exec()
+            .then((docs) => docs.map((doc) => doc.toObject()));
+        res.set('X-Total-Count', totalCount.toString())
+            .json(priceSpecifications);
     }
     catch (error) {
         next(error);
--- src/app/routes/priceSpecifications.ts
+++ src/app/routes/priceSpecifications.ts
@@ -3,6 +3,7 @@
  */
 import * as chevre from '@chevre/domain';
 import { Router } from 'express';
+import { CREATED, NO_CONTENT } from 'http-status';
 import * as mongoose from 'mongoose';
 
 import authentication from '../middlewares/authentication';
@@ -10,7 +11,9 @@ import permitScopes from '../middlewares/permitScopes';
 import validator from '../middlewares/validator';
 
 const priceSpecificationsRouter = Router();
+
 priceSpecificationsRouter.use(authentication);
+
 priceSpecificationsRouter.get(
     '/compoundPriceSpecification',
     permitScopes(['admin']),
@@ -38,4 +41,123 @@ priceSpecificationsRouter.get(
         }
     }
 );
+
+priceSpecificationsRouter.post(
+    '',
+    permitScopes(['admin']),
+    validator,
+    async (req, res, next) => {
+        try {
+            let priceSpecification: chevre.factory.priceSpecification.IPriceSpecification<any> = {
+                ...req.body
+            };
+            const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+            const doc = await priceSpecificationRepo.priceSpecificationModel.create(priceSpecification);
+
+            priceSpecification = doc.toObject();
+
+            res.status(CREATED)
+                .json(priceSpecification);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+priceSpecificationsRouter.get(
+    '/:id',
+    permitScopes(['admin']),
+    validator,
+    async (req, res, next) => {
+        try {
+            const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+            const doc = await priceSpecificationRepo.priceSpecificationModel.findById(req.params.id)
+                .exec();
+            if (doc === null) {
+                throw new chevre.factory.errors.NotFound('PriceSpecification');
+            }
+
+            const priceSpecification = doc.toObject();
+
+            res.json(priceSpecification);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+priceSpecificationsRouter.put(
+    '/:id',
+    permitScopes(['admin']),
+    validator,
+    async (req, res, next) => {
+        try {
+            const priceSpecification: chevre.factory.priceSpecification.IPriceSpecification<any> = {
+                ...req.body
+            };
+            const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+            await priceSpecificationRepo.priceSpecificationModel.findByIdAndUpdate(
+                req.params.id,
+                priceSpecification
+            )
+                .exec();
+
+            res.status(NO_CONTENT)
+                .end();
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+priceSpecificationsRouter.get(
+    '',
+    permitScopes(['admin']),
+    validator,
+    async (req, res, next) => {
+        try {
+            const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+            const searchCoinditions: chevre.factory.priceSpecification.ISearchConditions<any> = {
+                ...req.query,
+                // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
+                limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
+                page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
+            };
+
+            const totalCount = await priceSpecificationRepo.priceSpecificationModel.countDocuments({})
+                .setOptions({ maxTimeMS: 10000 })
+                .exec();
+
+            const query = priceSpecificationRepo.priceSpecificationModel.find(
+                {},
+                {
+                    __v: 0,
+                    createdAt: 0,
+                    updatedAt: 0
+                }
+            );
+            // tslint:disable-next-line:no-single-line-block-comment
+            /* istanbul ignore else */
+            if (searchCoinditions.limit !== undefined && searchCoinditions.page !== undefined) {
+                query.limit(searchCoinditions.limit)
+                    .skip(searchCoinditions.limit * (searchCoinditions.page - 1));
+            }
+            // tslint:disable-next-line:no-single-line-block-comment
+            /* istanbul ignore else */
+            if (searchCoinditions.sort !== undefined) {
+                query.sort(searchCoinditions.sort);
+            }
+
+            const priceSpecifications = await query.setOptions({ maxTimeMS: 10000 })
+                .exec()
+                .then((docs) => docs.map((doc) => doc.toObject()));
+
+            res.set('X-Total-Count', totalCount.toString())
+                .json(priceSpecifications);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 export default priceSpecificationsRouter;
