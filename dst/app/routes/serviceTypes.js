e9a19df4fbd1be4cdeb47064485c7423230b5a85
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-01-30 23:37:55 +0000

    ### Changed - サービスタイプをカテゴリーコードに統合

--- dst/app/routes/serviceTypes.js
+++ dst/app/routes/serviceTypes.js
@@ -27,15 +27,36 @@ serviceTypesRouter.post('', permitScopes_1.default(['admin']), ...[
     check_1.body('project')
         .not()
         .isEmpty()
-        .withMessage((_, __) => 'Required')
+        .withMessage(() => 'Required'),
+    check_1.body('project.id')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'Required')
+        .isString()
 ], validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const project = Object.assign(Object.assign({}, req.body.project), { typeOf: 'Project' });
-        let serviceType = Object.assign(Object.assign({}, req.body), { typeOf: 'ServiceType', id: '', project: project });
-        const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-        serviceType = yield serviceTypeRepo.save(serviceType);
+        const project = { id: req.body.project.id, typeOf: 'Project' };
+        // let serviceType: chevre.factory.serviceType.IServiceType = {
+        //     ...req.body,
+        //     typeOf: 'ServiceType',
+        //     id: '',
+        //     project: project
+        // };
+        // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+        // serviceType = await serviceTypeRepo.save(serviceType);
+        // res.status(CREATED)
+        //     .json(serviceType);
+        let categoryCode = Object.assign(Object.assign({}, req.body), { typeOf: 'CategoryCode', inCodeSet: {
+                typeOf: 'CategoryCodeSet',
+                identifier: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType
+            }, name: (typeof req.body.name === 'string')
+                ? { ja: req.body.name }
+                : req.body.name, project: project });
+        const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+        const doc = yield categoryCodeRepo.categoryCodeModel.create(categoryCode);
+        categoryCode = doc.toObject();
         res.status(http_status_1.CREATED)
-            .json(serviceType);
+            .json(Object.assign(Object.assign({}, categoryCode), { name: categoryCode.name.ja }));
     }
     catch (error) {
         next(error);
@@ -43,12 +64,25 @@ serviceTypesRouter.post('', permitScopes_1.default(['admin']), ...[
 }));
 serviceTypesRouter.get('', permitScopes_1.default(['admin', 'serviceTypes', 'serviceTypes.read-only']), validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-        const searchConditions = Object.assign(Object.assign({}, req.query), { 
+        // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+        // const searchConditions: chevre.factory.serviceType.ISearchConditions = {
+        //     ...req.query,
+        //     // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
+        //     limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
+        //     page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
+        // };
+        // const serviceTypes = await serviceTypeRepo.search(searchConditions);
+        // res.json(serviceTypes);
+        const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+        const searchConditions = Object.assign(Object.assign(Object.assign({}, req.query), (typeof req.query.name === 'string' && req.query.name.length > 0)
+            ? { name: { $regex: req.query.name } }
+            : undefined), { inCodeSet: { identifier: { $eq: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType } }, 
             // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
             limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1 });
-        const serviceTypes = yield serviceTypeRepo.search(searchConditions);
-        res.json(serviceTypes);
+        const categoryCodes = yield categoryCodeRepo.search(searchConditions);
+        res.json(categoryCodes.map((c) => {
+            return Object.assign(Object.assign({}, c), { name: c.name.ja });
+        }));
     }
     catch (error) {
         next(error);
@@ -56,19 +90,45 @@ serviceTypesRouter.get('', permitScopes_1.default(['admin', 'serviceTypes', 'ser
 }));
 serviceTypesRouter.get('/:id', permitScopes_1.default(['admin', 'serviceTypes', 'serviceTypes.read-only']), validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-        const serviceType = yield serviceTypeRepo.findById({ id: req.params.id });
-        res.json(serviceType);
+        // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+        // const serviceType = await serviceTypeRepo.findById({ id: req.params.id });
+        // res.json(serviceType);
+        const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+        const categoryCode = yield categoryCodeRepo.findById({ id: req.params.id });
+        res.json(Object.assign(Object.assign({}, categoryCode), { name: categoryCode.name.ja }));
     }
     catch (error) {
         next(error);
     }
 }));
-serviceTypesRouter.put('/:id', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+serviceTypesRouter.put('/:id', permitScopes_1.default(['admin']), ...[
+    check_1.body('project')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'Required'),
+    check_1.body('project.id')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'Required')
+        .isString()
+], validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const serviceType = Object.assign(Object.assign({}, req.body), { id: req.params.id });
-        const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-        yield serviceTypeRepo.save(serviceType);
+        const project = { id: req.body.project.id, typeOf: 'Project' };
+        // const serviceType: chevre.factory.serviceType.IServiceType = {
+        //     ...req.body,
+        //     id: req.params.id
+        // };
+        // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+        // await serviceTypeRepo.save(serviceType);
+        const categoryCode = Object.assign(Object.assign({}, req.body), { typeOf: 'CategoryCode', inCodeSet: {
+                typeOf: 'CategoryCodeSet',
+                identifier: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType
+            }, name: (typeof req.body.name === 'string')
+                ? { ja: req.body.name }
+                : req.body.name, project: project });
+        const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+        yield categoryCodeRepo.categoryCodeModel.findByIdAndUpdate(req.params.id, categoryCode)
+            .exec();
         res.status(http_status_1.NO_CONTENT)
             .end();
     }
@@ -78,8 +138,12 @@ serviceTypesRouter.put('/:id', permitScopes_1.default(['admin']), validator_1.de
 }));
 serviceTypesRouter.delete('/:id', permitScopes_1.default(['admin']), validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-        yield serviceTypeRepo.deleteById({
+        // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+        // await serviceTypeRepo.deleteById({
+        //     id: req.params.id
+        // });
+        const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+        yield categoryCodeRepo.deleteById({
             id: req.params.id
         });
         res.status(http_status_1.NO_CONTENT)
--- src/app/routes/serviceTypes.ts
+++ src/app/routes/serviceTypes.ts
@@ -22,25 +22,54 @@ serviceTypesRouter.post(
         body('project')
             .not()
             .isEmpty()
-            .withMessage((_, __) => 'Required')
+            .withMessage(() => 'Required'),
+        body('project.id')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'Required')
+            .isString()
     ],
     validator,
     async (req, res, next) => {
         try {
-            const project: chevre.factory.project.IProject = { ...req.body.project, typeOf: 'Project' };
+            const project: chevre.factory.project.IProject = { id: req.body.project.id, typeOf: 'Project' };
 
-            let serviceType: chevre.factory.serviceType.IServiceType = {
+            // let serviceType: chevre.factory.serviceType.IServiceType = {
+            //     ...req.body,
+            //     typeOf: 'ServiceType',
+            //     id: '',
+            //     project: project
+            // };
+
+            // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+            // serviceType = await serviceTypeRepo.save(serviceType);
+
+            // res.status(CREATED)
+            //     .json(serviceType);
+
+            let categoryCode: chevre.factory.categoryCode.ICategoryCode = {
                 ...req.body,
-                typeOf: 'ServiceType',
-                id: '',
+                typeOf: 'CategoryCode',
+                inCodeSet: {
+                    typeOf: 'CategoryCodeSet',
+                    identifier: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType
+                },
+                name: (typeof req.body.name === 'string')
+                    ? { ja: req.body.name }
+                    : req.body.name,
                 project: project
             };
 
-            const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-            serviceType = await serviceTypeRepo.save(serviceType);
+            const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+            const doc = await categoryCodeRepo.categoryCodeModel.create(categoryCode);
+
+            categoryCode = doc.toObject();
 
             res.status(CREATED)
-                .json(serviceType);
+                .json({
+                    ...categoryCode,
+                    name: (<any>categoryCode.name).ja
+                });
         } catch (error) {
             next(error);
         }
@@ -53,17 +82,39 @@ serviceTypesRouter.get(
     validator,
     async (req, res, next) => {
         try {
-            const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-            const searchConditions: chevre.factory.serviceType.ISearchConditions = {
+            // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+            // const searchConditions: chevre.factory.serviceType.ISearchConditions = {
+            //     ...req.query,
+            //     // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
+            //     limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
+            //     page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
+            // };
+
+            // const serviceTypes = await serviceTypeRepo.search(searchConditions);
+
+            // res.json(serviceTypes);
+
+            const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+
+            const searchConditions: chevre.factory.categoryCode.ISearchConditions = {
                 ...req.query,
+                ...(typeof req.query.name === 'string' && req.query.name.length > 0)
+                    ? { name: { $regex: req.query.name } }
+                    : undefined,
+                inCodeSet: { identifier: { $eq: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType } },
                 // tslint:disable-next-line:no-magic-numbers no-single-line-block-comment
                 limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
                 page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
             };
 
-            const serviceTypes = await serviceTypeRepo.search(searchConditions);
+            const categoryCodes = await categoryCodeRepo.search(searchConditions);
 
-            res.json(serviceTypes);
+            res.json(categoryCodes.map((c) => {
+                return {
+                    ...c,
+                    name: (<any>c.name).ja
+                };
+            }));
         } catch (error) {
             next(error);
         }
@@ -76,10 +127,16 @@ serviceTypesRouter.get(
     validator,
     async (req, res, next) => {
         try {
-            const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-            const serviceType = await serviceTypeRepo.findById({ id: req.params.id });
+            // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+            // const serviceType = await serviceTypeRepo.findById({ id: req.params.id });
 
-            res.json(serviceType);
+            // res.json(serviceType);
+
+            const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+
+            const categoryCode = await categoryCodeRepo.findById({ id: req.params.id });
+
+            res.json({ ...categoryCode, name: (<any>categoryCode.name).ja });
         } catch (error) {
             next(error);
         }
@@ -89,15 +146,47 @@ serviceTypesRouter.get(
 serviceTypesRouter.put(
     '/:id',
     permitScopes(['admin']),
+    ...[
+        body('project')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'Required'),
+        body('project.id')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'Required')
+            .isString()
+    ],
     validator,
     async (req, res, next) => {
         try {
-            const serviceType: chevre.factory.serviceType.IServiceType = {
+            const project: chevre.factory.project.IProject = { id: req.body.project.id, typeOf: 'Project' };
+
+            // const serviceType: chevre.factory.serviceType.IServiceType = {
+            //     ...req.body,
+            //     id: req.params.id
+            // };
+            // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+            // await serviceTypeRepo.save(serviceType);
+
+            const categoryCode: chevre.factory.categoryCode.ICategoryCode = {
                 ...req.body,
-                id: req.params.id
+                typeOf: 'CategoryCode',
+                inCodeSet: {
+                    typeOf: 'CategoryCodeSet',
+                    identifier: chevre.factory.categoryCode.CategorySetIdentifier.ServiceType
+                },
+                name: (typeof req.body.name === 'string')
+                    ? { ja: req.body.name }
+                    : req.body.name,
+                project: project
             };
-            const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-            await serviceTypeRepo.save(serviceType);
+            const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+            await categoryCodeRepo.categoryCodeModel.findByIdAndUpdate(
+                req.params.id,
+                categoryCode
+            )
+                .exec();
 
             res.status(NO_CONTENT)
                 .end();
@@ -113,8 +202,14 @@ serviceTypesRouter.delete(
     validator,
     async (req, res, next) => {
         try {
-            const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
-            await serviceTypeRepo.deleteById({
+            // const serviceTypeRepo = new chevre.repository.ServiceType(mongoose.connection);
+            // await serviceTypeRepo.deleteById({
+            //     id: req.params.id
+            // });
+
+            const categoryCodeRepo = new chevre.repository.CategoryCode(mongoose.connection);
+
+            await categoryCodeRepo.deleteById({
                 id: req.params.id
             });
 
