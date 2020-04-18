6d0cf992eccfa769a029bb8c81b3fadef0c7d72d
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-18 23:47:46 +0000

    ### Changed - COAイベントインポート処理を調整

--- dst/jobs/triggered/createImportEventsTask/run.js
+++ dst/jobs/triggered/createImportEventsTask/run.js
@@ -34,60 +34,65 @@ exports.default = (params) => __awaiter(void 0, void 0, void 0, function* () {
     // tslint:disable-next-line:no-magic-numbers
     10000);
     const connection = yield connectMongo_1.connectMongo({ defaultConnection: false });
-    const projectRepo = new chevre.repository.Project(connection);
-    const project = yield projectRepo.findById({ id: params.project.id });
-    const importEventsInWeeks = (typeof ((_a = project.settings) === null || _a === void 0 ? void 0 : _a.importEventsInWeeks) === 'number')
-        ? (_b = project.settings) === null || _b === void 0 ? void 0 : _b.importEventsInWeeks : 1;
-    const importEventsIntervalInMinutes = (typeof ((_c = project.settings) === null || _c === void 0 ? void 0 : _c.importEventsIntervalInMinutes) === 'number')
-        ? Math.min(Number((_d = project.settings) === null || _d === void 0 ? void 0 : _d.importEventsIntervalInMinutes), MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES)
-        // tslint:disable-next-line:no-magic-numbers
-        : MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES;
-    const job = new cron_1.CronJob(`*/${importEventsIntervalInMinutes} * * * *`, 
-    // tslint:disable-next-line:max-func-body-length
-    () => __awaiter(void 0, void 0, void 0, function* () {
-        if (!holdSingletonProcess) {
-            return;
-        }
-        const taskRepo = new chevre.repository.Task(connection);
-        const placeRepo = new chevre.repository.Place(connection);
-        const movieTheaters = yield placeRepo.searchMovieTheaters({
-            project: { ids: [params.project.id] }
-        });
-        const now = new Date();
-        const runsAt = now;
-        // 1週間ずつインポート
-        // tslint:disable-next-line:prefer-array-literal
-        yield Promise.all([...Array(Math.ceil(importEventsInWeeks / IMPORT_EVENTS_PER_WEEKS))].map((_, i) => __awaiter(void 0, void 0, void 0, function* () {
-            const importFrom = moment(now)
-                .add(i, 'weeks')
-                .toDate();
-            const importThrough = moment(importFrom)
-                .add(IMPORT_EVENTS_PER_WEEKS, 'weeks')
-                .toDate();
-            yield Promise.all(movieTheaters.map((movieTheater) => __awaiter(void 0, void 0, void 0, function* () {
-                try {
-                    const taskAttributes = {
-                        name: chevre.factory.taskName.ImportEventsFromCOA,
-                        status: chevre.factory.taskStatus.Ready,
-                        runsAt: runsAt,
-                        remainingNumberOfTries: 1,
-                        numberOfTried: 0,
-                        executionResults: [],
-                        data: {
-                            project: params.project,
-                            locationBranchCode: movieTheater.branchCode,
-                            importFrom: importFrom,
-                            importThrough: importThrough
-                        },
-                        project: params.project
-                    };
-                    yield taskRepo.save(taskAttributes);
-                }
-                catch (error) {
-                    console.error(error);
-                }
+    try {
+        const projectRepo = new chevre.repository.Project(connection);
+        const project = yield projectRepo.findById({ id: params.project.id });
+        const importEventsInWeeks = (typeof ((_a = project.settings) === null || _a === void 0 ? void 0 : _a.importEventsInWeeks) === 'number')
+            ? (_b = project.settings) === null || _b === void 0 ? void 0 : _b.importEventsInWeeks : 1;
+        const importEventsIntervalInMinutes = (typeof ((_c = project.settings) === null || _c === void 0 ? void 0 : _c.importEventsIntervalInMinutes) === 'number')
+            ? Math.min(Number((_d = project.settings) === null || _d === void 0 ? void 0 : _d.importEventsIntervalInMinutes), MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES)
+            // tslint:disable-next-line:no-magic-numbers
+            : MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES;
+        const job = new cron_1.CronJob(`*/${importEventsIntervalInMinutes} * * * *`, 
+        // tslint:disable-next-line:max-func-body-length
+        () => __awaiter(void 0, void 0, void 0, function* () {
+            if (!holdSingletonProcess) {
+                return;
+            }
+            const taskRepo = new chevre.repository.Task(connection);
+            const placeRepo = new chevre.repository.Place(connection);
+            const movieTheaters = yield placeRepo.searchMovieTheaters({
+                project: { ids: [params.project.id] }
+            });
+            const now = new Date();
+            const runsAt = now;
+            // 1週間ずつインポート
+            // tslint:disable-next-line:prefer-array-literal
+            yield Promise.all([...Array(Math.ceil(importEventsInWeeks / IMPORT_EVENTS_PER_WEEKS))].map((_, i) => __awaiter(void 0, void 0, void 0, function* () {
+                const importFrom = moment(now)
+                    .add(i, 'weeks')
+                    .toDate();
+                const importThrough = moment(importFrom)
+                    .add(IMPORT_EVENTS_PER_WEEKS, 'weeks')
+                    .toDate();
+                yield Promise.all(movieTheaters.map((movieTheater) => __awaiter(void 0, void 0, void 0, function* () {
+                    try {
+                        const taskAttributes = {
+                            name: chevre.factory.taskName.ImportEventsFromCOA,
+                            status: chevre.factory.taskStatus.Ready,
+                            runsAt: runsAt,
+                            remainingNumberOfTries: 1,
+                            numberOfTried: 0,
+                            executionResults: [],
+                            data: {
+                                project: params.project,
+                                locationBranchCode: movieTheater.branchCode,
+                                importFrom: importFrom,
+                                importThrough: importThrough
+                            },
+                            project: params.project
+                        };
+                        yield taskRepo.save(taskAttributes);
+                    }
+                    catch (error) {
+                        console.error(error);
+                    }
+                })));
             })));
-        })));
-    }), undefined, true);
-    debug('job started', job);
+        }), undefined, true);
+        debug('job started', job);
+    }
+    catch (error) {
+        console.error('createImportEventsTask:', error);
+    }
 });
--- src/jobs/triggered/createImportEventsTask/run.ts
+++ src/jobs/triggered/createImportEventsTask/run.ts
@@ -33,68 +33,72 @@ export default async (params: {
 
     const connection = await connectMongo({ defaultConnection: false });
 
-    const projectRepo = new chevre.repository.Project(connection);
-    const project = await projectRepo.findById({ id: params.project.id });
-    const importEventsInWeeks = (typeof (<any>project).settings?.importEventsInWeeks === 'number')
-        ? (<any>project).settings?.importEventsInWeeks : 1;
-    const importEventsIntervalInMinutes = (typeof (<any>project).settings?.importEventsIntervalInMinutes === 'number')
-        ? Math.min(Number((<any>project).settings?.importEventsIntervalInMinutes), MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES)
-        // tslint:disable-next-line:no-magic-numbers
-        : MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES;
+    try {
+        const projectRepo = new chevre.repository.Project(connection);
+        const project = await projectRepo.findById({ id: params.project.id });
+        const importEventsInWeeks = (typeof (<any>project).settings?.importEventsInWeeks === 'number')
+            ? (<any>project).settings?.importEventsInWeeks : 1;
+        const importEventsIntervalInMinutes = (typeof (<any>project).settings?.importEventsIntervalInMinutes === 'number')
+            ? Math.min(Number((<any>project).settings?.importEventsIntervalInMinutes), MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES)
+            // tslint:disable-next-line:no-magic-numbers
+            : MAX_IMPORT_EVENTS_INTERVAL_IN_MINUTES;
 
-    const job = new CronJob(
-        `*/${importEventsIntervalInMinutes} * * * *`,
-        // tslint:disable-next-line:max-func-body-length
-        async () => {
-            if (!holdSingletonProcess) {
-                return;
-            }
+        const job = new CronJob(
+            `*/${importEventsIntervalInMinutes} * * * *`,
+            // tslint:disable-next-line:max-func-body-length
+            async () => {
+                if (!holdSingletonProcess) {
+                    return;
+                }
 
-            const taskRepo = new chevre.repository.Task(connection);
-            const placeRepo = new chevre.repository.Place(connection);
+                const taskRepo = new chevre.repository.Task(connection);
+                const placeRepo = new chevre.repository.Place(connection);
 
-            const movieTheaters = await placeRepo.searchMovieTheaters({
-                project: { ids: [params.project.id] }
-            });
-            const now = new Date();
-            const runsAt = now;
+                const movieTheaters = await placeRepo.searchMovieTheaters({
+                    project: { ids: [params.project.id] }
+                });
+                const now = new Date();
+                const runsAt = now;
 
-            // 1週間ずつインポート
-            // tslint:disable-next-line:prefer-array-literal
-            await Promise.all([...Array(Math.ceil(importEventsInWeeks / IMPORT_EVENTS_PER_WEEKS))].map(async (_, i) => {
-                const importFrom = moment(now)
-                    .add(i, 'weeks')
-                    .toDate();
-                const importThrough = moment(importFrom)
-                    .add(IMPORT_EVENTS_PER_WEEKS, 'weeks')
-                    .toDate();
+                // 1週間ずつインポート
+                // tslint:disable-next-line:prefer-array-literal
+                await Promise.all([...Array(Math.ceil(importEventsInWeeks / IMPORT_EVENTS_PER_WEEKS))].map(async (_, i) => {
+                    const importFrom = moment(now)
+                        .add(i, 'weeks')
+                        .toDate();
+                    const importThrough = moment(importFrom)
+                        .add(IMPORT_EVENTS_PER_WEEKS, 'weeks')
+                        .toDate();
 
-                await Promise.all(movieTheaters.map(async (movieTheater) => {
-                    try {
-                        const taskAttributes: chevre.factory.task.importEventsFromCOA.IAttributes = {
-                            name: chevre.factory.taskName.ImportEventsFromCOA,
-                            status: chevre.factory.taskStatus.Ready,
-                            runsAt: runsAt,
-                            remainingNumberOfTries: 1,
-                            numberOfTried: 0,
-                            executionResults: [],
-                            data: {
-                                project: params.project,
-                                locationBranchCode: movieTheater.branchCode,
-                                importFrom: importFrom,
-                                importThrough: importThrough
-                            },
-                            project: params.project
-                        };
-                        await taskRepo.save(taskAttributes);
-                    } catch (error) {
-                        console.error(error);
-                    }
+                    await Promise.all(movieTheaters.map(async (movieTheater) => {
+                        try {
+                            const taskAttributes: chevre.factory.task.importEventsFromCOA.IAttributes = {
+                                name: chevre.factory.taskName.ImportEventsFromCOA,
+                                status: chevre.factory.taskStatus.Ready,
+                                runsAt: runsAt,
+                                remainingNumberOfTries: 1,
+                                numberOfTried: 0,
+                                executionResults: [],
+                                data: {
+                                    project: params.project,
+                                    locationBranchCode: movieTheater.branchCode,
+                                    importFrom: importFrom,
+                                    importThrough: importThrough
+                                },
+                                project: params.project
+                            };
+                            await taskRepo.save(taskAttributes);
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    }));
                 }));
-            }));
-        },
-        undefined,
-        true
-    );
-    debug('job started', job);
+            },
+            undefined,
+            true
+        );
+        debug('job started', job);
+    } catch (error) {
+        console.error('createImportEventsTask:', error);
+    }
 };
