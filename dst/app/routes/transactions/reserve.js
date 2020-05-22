67e5e3a61db77649f3801bb1370c674da3000718
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-05-22 02:16:41 +0000

    ### Changed - 予約取引開始時に座席オファーを指定できるように調整

--- dst/app/routes/transactions/reserve.js
+++ dst/app/routes/transactions/reserve.js
@@ -44,18 +44,43 @@ reserveTransactionsRouter.post('/start', permitScopes_1.default(['admin', 'trans
 }, validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const projectRepo = new chevre.repository.Project(mongoose.connection);
-        const transactionRepo = new chevre.repository.Transaction(mongoose.connection);
         const transactionNumberRepo = new chevre.repository.TransactionNumber(redis.getClient());
+        const eventRepo = new chevre.repository.Event(mongoose.connection);
+        const placeRepo = new chevre.repository.Place(mongoose.connection);
+        const priceSpecificationRepo = new chevre.repository.PriceSpecification(mongoose.connection);
+        const taskRepo = new chevre.repository.Task(mongoose.connection);
+        const transactionRepo = new chevre.repository.Transaction(mongoose.connection);
+        const offerRepo = new chevre.repository.Offer(mongoose.connection);
+        const offerCatalogRepo = new chevre.repository.OfferCatalog(mongoose.connection);
+        const eventAvailabilityRepo = new chevre.repository.itemAvailability.ScreeningEvent(redis.getClient());
+        const offerRateLimitRepo = new chevre.repository.rateLimit.Offer(redis.getClient());
+        const productRepo = new chevre.repository.Product(mongoose.connection);
+        const reservationRepo = new chevre.repository.Reservation(mongoose.connection);
         const project = Object.assign(Object.assign({}, req.body.project), { typeOf: 'Project' });
-        const transaction = yield chevre.service.transaction.reserve.start(Object.assign({ project: project, typeOf: chevre.factory.transactionType.Reserve, agent: Object.assign({}, req.body.agent
-            // id: (req.body.agent.id !== undefined) ? req.body.agent.id : req.user.sub,
-            ), object: Object.assign({}, req.body.object), expires: moment(req.body.expires)
+        const transaction = yield chevre.service.transaction.reserve.start(Object.assign({ project: project, typeOf: chevre.factory.transactionType.Reserve, agent: req.body.agent, object: req.body.object, expires: moment(req.body.expires)
                 .toDate() }, (typeof req.body.transactionNumber === 'string') ? { transactionNumber: req.body.transactionNumber } : undefined))({
             project: projectRepo,
+            eventAvailability: eventAvailabilityRepo,
+            event: eventRepo,
+            offer: offerRepo,
+            offerCatalog: offerCatalogRepo,
+            offerRateLimit: offerRateLimitRepo,
+            place: placeRepo,
+            priceSpecification: priceSpecificationRepo,
+            product: productRepo,
+            reservation: reservationRepo,
+            task: taskRepo,
             transaction: transactionRepo,
             transactionNumber: transactionNumberRepo
         });
-        res.json(transaction);
+        // レスポンスデータ量が大きくて不要な場合、受け取らない選択ができるように
+        if (req.query.expectsNoContent === '1') {
+            res.status(http_status_1.NO_CONTENT)
+                .end();
+        }
+        else {
+            res.json(transaction);
+        }
     }
     catch (error) {
         next(error);
